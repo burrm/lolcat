de30e20ae3b367189482cdfd0f93d39eaf099fa7
burrm <burrm@colorado.edu>
2016-01-19 11:13:14 -0700

    Started SPC work, version 1.1 started

---
+++ R/spc.controlviolation.runs.R
@@ -0,0 +1,36 @@
+spc.controlviolation.run.above <- function(x, run.length=8, center.line = 0, ...) {
+  ret <- logical(0)
+  
+  observed.run.lengths <- rle(x > center.line)
+  
+  for (i in 1:length(observed.run.lengths$lengths)) {
+    is.run.violation <- observed.run.lengths$lengths[i] >= run.length & observed.run.lengths$values[i]
+    ret <- c(ret, rep(is.run.violation, observed.run.lengths$lengths[i]))
+  }
+  
+  ret  
+}
+
+
+spc.controlviolation.run.below <- function(x, run.length=8, center.line = 0, ...) {
+  ret <- logical(0)
+  
+  observed.run.lengths <- rle(x < center.line)
+  
+  for (i in 1:length(observed.run.lengths$lengths)) {
+    is.run.violation <- observed.run.lengths$lengths[i] >= run.length & observed.run.lengths$values[i]
+    ret <- c(ret, rep(is.run.violation, observed.run.lengths$lengths[i]))
+  }
+  
+  ret  
+}
+
+
+spc.controlviolation.runs <- function(x, run.length=8, center.line = 0, ...) {
+  ret.1 <- spc.controlviolation.run.above(x = x, run.length = run.length, center.line = center.line)
+  ret.2 <- spc.controlviolation.run.below(x = x, run.length = run.length, center.line = center.line)
+  
+  ret.1 | ret.2
+}
+
+
---
+++ R/spc.individuals.chart.R
@@ -0,0 +1,5 @@
+spc.individuals.chart<-function(x, type="b", main = "Individuals Chart", xlab = "Time", ylab = "Criterion Measure", col= "blue", pch=19, ...) {
+  plot(1:length(x), type=type, x, main=main, xlab = xlab, ylab = ylab, col=col, pch=pch,  ...)
+}
+
+spc.x.chart<-spc.individuals.chart
