79c04a088ec78713fafb3c850df9aa033c5a0cc6
burrm <burrm@colorado.edu>
2016-06-05 21:56:22 -0600

    added one sample variance test accepting a vector

---
+++ R/variance.test.onesample.R
@@ -0,0 +1,16 @@
+variance.test.onesample<-function(g1
+                                  ,h0.variance = 1
+                                  ,alternative = c("two.sided","less","greater")
+                                  ,conf.level = 0.95) {
+  
+  g1 <- na.omit(g1)
+  sample.variance <- var(g1)
+  sample.size <- length(g1)
+  
+  variance.test.onesample.simple(sample.variance
+                                 ,sample.size
+                                 ,h0.variance = h0.variance
+                                 ,alternative = alternative
+                                 ,conf.level = conf.level)
+
+}
