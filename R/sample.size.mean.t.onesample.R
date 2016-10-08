d24a5fba6868245366eea573cc18295b4d5df1cf
burrm <burrm@colorado.edu>
2016-10-08 08:08:24 -0600

    Departure from MVP sample size. Implementation closer to GPower description

--- R/sample.size.mean.t.onesample.R
+++ R/sample.size.mean.t.onesample.R
@@ -5,61 +5,58 @@ sample.size.mean.t.onesample <- function(effect.size
                                          ,alternative = c("two.sided","less","greater")
                                          ,details = TRUE
                                          ,power.from.actual = F #report parameter power instead of true power
+                                         ,max.iter = 1000000
 ) {
   se.est <- sqrt(variance.est)
   
-  z.res <- sample.size.mean.z.onesample(effect.size = effect.size
-                                        ,variance = variance.est
-                                        ,alpha = alpha
-                                        ,beta = beta
-                                        ,alternative = alternative
-                                        ,details = T
-                                        ,power.from.actual = power.from.actual
-  )
+  df <- 1
   
-  df <- max(z.res$sample.size-1, 1)
   
-  n_gen <- function(df) {
-    t_alpha <- qt(ifelse(alternative[1] == "two.sided",alpha/2,alpha), df= df, lower.tail = F)
-    t_beta  <- qt(beta, df = df, lower.tail = F)
-
-    ((t_alpha + t_beta)*se.est/effect.size)^2    
-  }
-  
-  n <- z.res$sample.size
-  
-  while (n != ceiling(n_gen(df))) {
-    df <- df+1
-    n <- n+1
-  }
-    
-  actual <- n_gen(df) #-> misleading?
-
   if (effect.size > 0 && alternative == "less") {
     df <- NA
     n <- NA
     actual <- NA
-  }
-
-  if (effect.size < 0 && alternative == "greater") {
+  } else if (effect.size < 0 && alternative == "greater") {
     df <- NA
     n <- NA
     actual <- NA
-  }
-  
-    
-  if (power.from.actual) {
-    power <- 1- beta
   } else {
-    power <- power.mean.t.onesample(sample.size = n, 
+    target.power <- 1-beta
+    
+    power <- power.mean.t.onesample(sample.size = df + 1, 
                                     effect.size = effect.size,
                                     variance = variance.est,
                                     alpha=alpha,
                                     alternative = alternative,
                                     details=FALSE
     )
+    n <- df +1
+    actual <- n
     
-    beta <- 1-power
+        
+    while (power < target.power & df <= max.iter) {
+      df <- df+1
+      n <- df +1
+      actual <- n
+
+      power <- power.mean.t.onesample(sample.size = n, 
+                                      effect.size = effect.size,
+                                      variance = variance.est,
+                                      alpha=alpha,
+                                      alternative = alternative,
+                                      details=FALSE
+      )
+      
+      
+    }
+    
+    
+  }
+  
+    
+  if (power.from.actual) {
+    power <- 1- beta
+  } else {
     
   }
   
