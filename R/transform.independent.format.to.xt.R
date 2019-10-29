8ee39bdf5cb67af8191019a68091857e845faaa9
Mike Burr <burrm@colorado.edu>
2019-10-28 22:07:03 -0600

    add row/col label to xt

--- R/transform.independent.format.to.xt.R
+++ R/transform.independent.format.to.xt.R
@@ -3,7 +3,10 @@ transform.independent.format.to.xt <-function(x_row
                                               ,weight = rep(1,length(x_row)) 
                                               ,na.rm = T
                                               ,sort.names = T
-                                              ) {
+                                              ,x_row_name = "row_label"
+                                              ,x_col_name = "col_label"
+) {
+  
   if (na.rm) {
     d <- data.frame(x_row = x_row, x_col = x_col)
     d <- na.omit(d)
@@ -21,8 +24,16 @@ transform.independent.format.to.xt <-function(x_row
   }
   
   ret <- matrix(0, nrow=length(unique_x_row), ncol=length(unique_x_col))
-  rownames(ret) <- unique_x_row
-  colnames(ret) <- unique_x_col
+
+  if (x_row_name == x_col_name) {
+    x_col_name <- paste0(x_col_name, ".1")
+  }
+    
+  newnames <- list()
+  newnames[[x_row_name]] <- unique_x_row
+  newnames[[x_col_name]] <- unique_x_col
+  
+  dimnames(ret) <- newnames
   
   for (i in 1:length(unique_x_row)) {
     for (j in 1:length(unique_x_col)) {
