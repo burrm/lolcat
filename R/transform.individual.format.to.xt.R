d78d429d46150f33a5e29493eead63eb06dbb99a
Mike Burr <burrm@colorado.edu>
2019-10-29 19:33:04 -0600

    alias update

--- R/transform.individual.format.to.xt.R
+++ R/transform.individual.format.to.xt.R
@@ -1,12 +1,5 @@
-transform.individual.format.to.xt <-function(x_row
-                                              ,x_col
-                                              ,weight = rep(1,length(x_row)) 
-                                              ,na.rm = T
-                                              ,sort.names = T
+transform.individual.format.to.xt <-function(
+  ...
 ) {
-  transform.independent.format.to.xt(x_row = x_row
-                                     ,x_col = x_col
-                                     ,weight = weight 
-                                     ,na.rm = na.rm
-                                     ,sort.names = sort.names)
+  transform.independent.format.to.xt(...)
 }
