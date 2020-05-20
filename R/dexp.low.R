c77368d57085667c7c539c4d9e6d135a4e7ab231
Mike Burr <burrm@colorado.edu>
2020-05-20 12:04:20 -0600

    dexp.low change

--- R/dexp.low.R
+++ R/dexp.low.R
@@ -11,27 +11,43 @@ pexp.low <- function(
     q, 
     rate = 1, 
     low = 0, 
+    mean = NA,
     lower.tail = TRUE, 
     log.p = FALSE
 ) {
-    pexp(q = q-low, rate = rate, lower.tail = lower.tail, log.p = log.p)
+    if (is.na(mean)) {
+        pexp(q = q-low, rate = rate, lower.tail = lower.tail, log.p = log.p)
+    } else {
+        pexp(q = q-low, rate = 1/(mean-low), lower.tail = lower.tail, log.p = log.p)
+    }
 }
 
 qexp.low <- function(
     p, 
     rate = 1, 
     low = 0, 
+    mean = NA,
     lower.tail = TRUE, 
     log.p = FALSE
 ) {
-    qexp(p = p, rate = rate, lower.tail = lower.tail, log.p = log.p) + low
+    if (is.na(mean)) {
+        qexp(p = p, rate = rate, lower.tail = lower.tail, log.p = log.p) + low
+    } else {
+        qexp(p = p, rate = 1/(mean-low), lower.tail = lower.tail, log.p = log.p) + low
+    }
+    
 }
 
 rexp.low <- function(
     n, 
     rate = 1, 
-    low = 0
+    low = 0,
+    mean = NA
 ) {
-    rexp(n, rate = rate) + low
+    if (is.na(mean)) {
+        rexp(n, rate = rate) + low
+    } else {
+        rexp(n, rate = 1/(mean-low)) + low
+    }
 }
 
